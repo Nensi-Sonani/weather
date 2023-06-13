1edbb0811f5c022983e42d4312d373c7f3117f2d
Nensi Sonani <nrsonani13@gmail.com>
2023-06-13 17:11:21 +0530

    weather

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-// document.getElementById("btn").addEventListener("click",function(){
-//     navigator.geolocation.getCurrentPosition((position)=>{
-//         let lat = position.coords.latitude
-//         let lan = position.coords.longitude
-//         console.log(lat,lan);
-//     });
-// });
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1a26708ce353c0fd4ceb97aaa7863ce3`)
-.then((Response)=>Response.json())
-.then(data=>console.log(data.main.temp));
+// calling api with city name 
+
+const getweather=(city)=>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1a26708ce353c0fd4ceb97aaa7863ce3`)
+    .then((response)=>response.json())
+    .then(data=>console.log(data));
+}
+
+// function for getting city name 
+document.getElementById("location").addEventListener("keypress",function(e){
+    // console.log(e.key);
+    if(e.key == "Enter"){
+        let city = document.getElementById("location").value
+        console.log(city);
+        getweather(city)
+    }
+})
+
+
+// calling api with latitude and longitude
+
+const get=(lat,lon)=>{
+    console.log(lat,lon);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1a26708ce353c0fd4ceb97aaa7863ce3&units=metric`)
+    .then((response)=>response.json())
+    .then(data=>console.log(data));
+}
+
+
+//function for getting latitude and longitude
+
+document.getElementById("btn").addEventListener("click",function(){
+    navigator.geolocation.getCurrentPosition((position)=>{
+        let lat = position.coords.latitude
+        let lan = position.coords.longitude
+        console.log(lat,lan);
+        get(lat,lan)
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
